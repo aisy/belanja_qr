04bb39b786bb15f40dc1cf3fcf2df3b15f7b684d
Aisy Rozsidhy <aisymuhammad@gmail.com>
2017-02-14 16:45:33 +0700

    menambah halaman utama

---
+++ application/views/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Kasir</title>
+
+    <link rel="stylesheet" href="<?= base_url('ass/font-awesome/css/font-awesome.css') ?>">
+
+    <link rel="stylesheet" href="<?= base_url('ass/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('ass/css/mdb.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('ass/css/style.css') ?>">
+
+  </head>
+
+</html>
