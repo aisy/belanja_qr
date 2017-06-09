b1fe72953081f1117b2911e3abd2d07f311ce7c4
Aisy Rozsidhy <aisymuhammad@gmail.com>
2017-06-09 21:13:14 +0700

    add profil API, and add tanggal_transaksi on json at transaksi

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -73,6 +73,19 @@ public function __construct(){
       exit;
   }
 
+  public function get_user($id){
+
+    $data = $this->Model_user->get_id($id);
+
+    $this->output
+      ->set_status_header(200)
+      ->set_content_type('application/json', 'utf-8')
+      ->set_output(json_encode($data, JSON_PRETTY_PRINT))
+      ->_display();
+      exit;
+
+  }
+
 }
 
 /* End of file controllername.php */
